603f122fa1aea00509543935628ab0f8e9c77ffc
Anders Djerf <37343667+istorp@users.noreply.github.com>
2020-03-30 15:14:57 +0200

    istorp

---
+++ Source/the-ludo game-grupp-6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace the_ludo_game_grupp_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+        }
+    }
+    public class player
+    {
+        public int MyProperty { get; set; }
+    }
+}
