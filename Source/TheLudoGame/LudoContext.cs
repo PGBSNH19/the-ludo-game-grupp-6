ad23a99ff514e2b848d3f182c0182af418f1157f
Anders Djerf <37343667+istorp@users.noreply.github.com>
2020-04-01 11:22:55 +0200

    istorp

---
+++ Source/TheLudoGame/LudoContext.cs
@@ -0,0 +1,19 @@
+﻿using GameEngine;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheLudoGame
+{
+    class LudoContext: DbContext
+
+    {
+       
+        public DbSet<BoardSquare> BoardSquares { get; set; }
+        public DbSet<Board> Boards { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Player> Players { get; set; }
+       
+    }
+}
