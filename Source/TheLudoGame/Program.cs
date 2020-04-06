8cb6948482e8c73db6d3ef3d5711fbcea35422cf
norsh_6pstx7m <norsh_6pstx7m@LAPTOP-J0PKJ9I1>
2020-04-06 10:58:07 +0200

    Conflict

--- Source/TheLudoGame/Program.cs
+++ Source/TheLudoGame/Program.cs
@@ -15,10 +15,7 @@ namespace TheLudoGame
                 .Build();
             var context = new LudoContext(configuration);
 
-            var game = new Game()
-                .AddPlayer(new RedPlayer())
-                .AddPlayer(new BluePlayer())
-                .AddPlayer(new RedPlayer());
+            var game = new Game();
 
             Console.WriteLine(configuration.GetConnectionString("DefaultConnection"));
             Console.ReadLine();
