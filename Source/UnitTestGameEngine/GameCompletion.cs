99cac19e93bca4a2f2535012c828421357504aa6
Nor Shiervani <nor.shier97@gmail.com>
2020-04-12 19:55:45 +0200

    Fixes to the unit tests in GameCompletion.cs .

--- Source/UnitTestGameEngine/GameCompletion.cs
+++ Source/UnitTestGameEngine/GameCompletion.cs
@@ -9,11 +9,13 @@ namespace UnitTestGameEngine
         [TestMethod]
         public void Game_Not_Finished_Condition()
         {
-            var player = new Player { Score = 0 };
+            var player = new Player { PlayerType = PlayerType.Blue, Score = 0 };
+            var secondPlayer = new Player { PlayerType = PlayerType.Green, Score = 1 };
             var game = new Game()
                 .New()
-                .Build()
-                .AddPlayer(player);
+                .AddPlayer(player)
+                .AddPlayer(secondPlayer)
+                .Build();
 
             var expectedResult = false;
 
@@ -23,11 +25,13 @@ namespace UnitTestGameEngine
         [TestMethod]
         public void Game_Finished_Condition()
         {
-            var player = new Player { Score = 4 };
+            var player = new Player { PlayerType = PlayerType.Blue, Score = 4 };
+            var secondPlayer = new Player { PlayerType = PlayerType.Green, Score = 2 };
             var game = new Game()
                 .New()
-                .Build()
-                .AddPlayer(player);
+                .AddPlayer(player)
+                .AddPlayer(secondPlayer)
+                .Build();
 
             var expectedResult = true;
 
